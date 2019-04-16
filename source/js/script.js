90f8c106ea1a18bbcde9441d28a630637cfc311e
alekseyburn <alekseyburn@gmail.com>
2019-04-16 23:17:28 +0300

    задал макс ширину, переделал скрипт меню, сделал открытие модалки, вынес кнопку отдельно

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,34 @@
-  var navMain = document.querySelector('.page-header');
-  var navToggle = document.querySelector('.page-header__toggle');
+  var navMain = document.querySelector(".page-header");
+  var navToggle = document.querySelector(".page-header__toggle");
 
-  navMain.classList.remove('page-header--nojs');
+  navMain.classList.remove("page-header--nojs");
 
-  navToggle.addEventListener('click', function() {
-    if (navMain.classList.contains('page-header--menu-closed')) {
-      navMain.classList.remove('page-header--menu-closed');
-      navMain.classList.add('page-header--menu-opened');
-    } else {
-      navMain.classList.add('page-header--menu-closed');
-      navMain.classList.remove('page-header--menu-opened');
-    }
+  navToggle.addEventListener("click", function() {
+    navMain.classList.toggle("page-header--menu-opened");
   });
+
+  var link = document.querySelector(".weekly-good__button");
+  var popup = document.querySelector(".cart");
+  var overlay = document.querySelector(".overlay");
+
+  link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("cart--show");
+  overlay.classList.add("overlay--show");
+});
+
+  overlay.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("cart--show");
+  overlay.classList.remove("overlay--show");
+});
+
+  window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("cart--show")) {
+      popup.classList.remove("cart--show");
+      overlay.classList.remove("overlay--show");
+    }
+  }
+})
