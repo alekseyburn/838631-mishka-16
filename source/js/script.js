acf4faaa7213ef5dd94627ccc30ddffd95f5e873
alekseyburn <alekseyburn@gmail.com>
2019-04-19 01:02:19 +0300

    выполнил выравнивание в скрипте

--- source/js/script.js
+++ source/js/script.js
@@ -1,40 +1,47 @@
-  "use strict";
+"use strict";
 
-  (function () {
-    let navMain = document.querySelector(".page-header");
-    let navToggle = document.querySelector(".page-header__toggle");
+(function () {
+  let navMain = document.querySelector(".page-header");
+  let navToggle = document.querySelector(".page-header__toggle");
 
-  navMain.classList.remove("page-header--nojs"); //удаляем класс nojs, если js доступен
+  //удаляем класс nojs, если js доступен
+  navMain.classList.remove("page-header--nojs");
 
   navToggle.addEventListener("click", function() {
-    navMain.classList.toggle("page-header--menu-opened"); //открываем/закрываем меню при нажатии на бургер/крестик
+    //открываем/закрываем меню при нажатии на бургер/крестик
+    navMain.classList.toggle("page-header--menu-opened");
   });
 
   let link = document.querySelector(".weekly-good__button");
   let popup = document.querySelector(".cart");
   let overlay = document.querySelector(".overlay");
 
-  let modalShow = function () { //функция открытия модального окна и оверлея
+  //функция открытия модального окна и оверлея
+  let modalShow = function () {
     popup.classList.add("cart--show");
     overlay.classList.add("overlay--show");
   };
 
-  let modalClose = function () { //функция закрытия модального окна и оверлея
+  //функция закрытия модального окна и оверлея
+  let modalClose = function () {
     popup.classList.remove("cart--show");
     overlay.classList.remove("overlay--show");
   };
 
-  link.addEventListener("click", function (event) { //открываем модалку при нажатии на кнопку "заказать"
+  //открываем модалку при нажатии на кнопку "заказать"
+  link.addEventListener("click", function (event) {
     event.preventDefault();
     modalShow();
   });
 
-  overlay.addEventListener("click", function (event) { //закрываем модалку при нажатии на оверлей
+  //закрываем модалку при нажатии на оверлей
+  overlay.addEventListener("click", function (event) {
     event.preventDefault();
     modalClose();
   });
 
-  document.addEventListener("keyup", function (event) { //закрываем модалку при нажатии на esc
+  //закрываем модалку при нажатии на esc
+  document.addEventListener("keyup", function (event) {
     if (event.defaultPrevented) {
       return;
     }
@@ -48,5 +55,4 @@
       }
     }
   });
-
 })();
