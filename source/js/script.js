b0c16327a1e970dba337549d7ddbfa769e40aa09
alekseyburn <alekseyburn@gmail.com>
2019-04-20 22:08:47 +0300

    сделал слайдер отзывов

--- source/js/script.js
+++ source/js/script.js
@@ -55,4 +55,56 @@
       }
     }
   });
+
+
+
+  //слайдер
+  let slideIndex = 1;
+  showSlides(slideIndex);
+  //следующий слайд
+  function plusSlide() {
+    showSlides(slideIndex += 1);
+  }
+  //предыдущий слайд
+  function minusSlide() {
+    showSlides(slideIndex -= 1);
+  }
+  //текущий слайд
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  //функция показа слайда
+  function showSlides(n) {
+    let i;
+    let slides = document.querySelectorAll(".reviews__item");
+
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+
+    if (n < 1) {
+      slideIndex = slides.length
+    }
+
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+  }
+  //назначение переключателей на кнопки
+  var slideLeft = document.querySelector(".reviews__change-btn--prev");
+  var slideRight = document.querySelector(".reviews__change-btn--next");
+
+  slideLeft.addEventListener("click", function (event) {
+    event.preventDefault();
+    minusSlide();
+  });
+
+  slideRight.addEventListener("click", function (event) {
+    event.preventDefault();
+    plusSlide();
+  });
+
 })();
