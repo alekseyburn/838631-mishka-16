8ded0a3a0604ffc597514ed4ce1898ec51bfb11f
alekseyburn <alekseyburn@gmail.com>
2019-04-24 19:48:36 +0300

    вынес svg спрайт в отдельный файл, добавил полифил для ie11

--- source/js/iepolyfills.js
+++ source/js/iepolyfills.js
@@ -1,11 +1,12 @@
 "use strict";
 
 (function () {
-  //определяем, что браузер - ie11, загружаем picturefill из cdn
+  //определяем, что браузер - ie11, загружаем полифилы из cdn
   if(/Trident.*rv:/.test(navigator.userAgent)) {
     document.write('<script src="//cdnjs.cloudflare.com/ajax/libs/picturefill/3.0.3/picturefill.min.js" onerror="someFunction()"><\/script>');
+    document.write('<script src="//cdnjs.cloudflare.com/ajax/libs/svgxuse/1.2.6/svgxuse.min.js" onerror="someFunction()"><\/script>');
   }
-  //если cdn недоступен, загружаем picturefill локально
+  //если cdn недоступен, загружаем полифилы локально
   function loadFile(file) {
     let fileRef = document.createElement('script');
 
@@ -16,5 +17,9 @@
 
   function someFunction() {
     loadFile('picturefill.min.js');
+  }
+
+  function someFunction() {
+    loadFile('svgxuse.min.js');
   }
 })();
