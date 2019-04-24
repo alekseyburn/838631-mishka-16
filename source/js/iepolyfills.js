1790959607ca272f8ab59466444c0818ee72f589
alekseyburn <alekseyburn@gmail.com>
2019-04-24 22:39:01 +0300

    поправил скрипт polyfills и добавил спрайты в catalog и form

--- source/js/iepolyfills.js
+++ source/js/iepolyfills.js
@@ -1,12 +1,13 @@
 "use strict";
 
 (function () {
-  //определяем, что браузер - ie11, загружаем полифилы из cdn
+  //определяем, что браузер - ie11, загружаем полифил из cdn
   if(/Trident.*rv:/.test(navigator.userAgent)) {
-    document.write('<script src="//cdnjs.cloudflare.com/ajax/libs/picturefill/3.0.3/picturefill.min.js" onerror="someFunction()"><\/script>');
-    document.write('<script src="//cdnjs.cloudflare.com/ajax/libs/svgxuse/1.2.6/svgxuse.min.js" onerror="someFunction()"><\/script>');
+    document.write('<script src="//cdnjs.cloudflare.com/ajax/libs/picturefill/3.0.3/picturefill.min.js" onerror="loadPicturefill()"><\/script>');
+    document.write('<script src="//cdnjs.cloudflare.com/ajax/libs/svgxuse/1.2.6/svgxuse.min.js" onerror="loadSvgxuse()"><\/script>');
   }
-  //если cdn недоступен, загружаем полифилы локально
+
+  //если cdn недоступен, загружаем полифил локально
   function loadFile(file) {
     let fileRef = document.createElement('script');
 
@@ -15,11 +16,11 @@
     document.getElementsByTagName("head")[0].appendChild(fileRef);
   }
 
-  function someFunction() {
+  function loadPicturefill() {
     loadFile('picturefill.min.js');
   }
 
-  function someFunction() {
+  function loadSvgxuse() {
     loadFile('svgxuse.min.js');
   }
 })();
